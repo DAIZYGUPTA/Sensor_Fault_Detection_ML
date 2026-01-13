ece7d956baa3168183f7c33074b3662547b0cfd1
DAIZYGUPTA <guptadaizy2495@gmail.com>
2026-01-13 19:09:47 +0530

    updated dataingestion file and upload_data.py file

--- src/utils/main.utils.py
+++ src/utils/main.utils.py
@@ -18,31 +18,26 @@ class MainUtils:
         try:
             with open(filename, "rb") as yaml_file:
                 return yaml.safe_load(yaml_file)
-
         except Exception as e:
             raise CustomException(e, sys) from e
 
     def read_schema_config_file(self) -> dict:
         try:
             schema_config = self.read_yaml_file(os.path.join("config", "schema.yaml"))
-
             return schema_config
-
         except Exception as e:
             raise CustomException(e, sys) from e
 
     @staticmethod
     def save_object(file_path: str, obj: object) -> None:
         logging.info("Entered the save_object method of MainUtils class")
-
         try:
             with open(file_path, "wb") as file_obj:
                 pickle.dump(obj, file_obj)
             logging.info("Exited the save_object method of MainUtils class")
-
         except Exception as e:
             raise CustomException(e, sys) from e
-
+        
     @staticmethod
     def load_object(file_path: str) -> object:
         logging.info("Entered the load_object method of MainUtils class")
@@ -51,9 +46,7 @@ class MainUtils:
                 obj = pickle.load(file_obj)
 
             logging.info("Exited the load_object method of MainUtils class")
-
             return obj
-
         except Exception as e:
             raise CustomException(e, sys) from e
    
@@ -65,6 +58,4 @@ class MainUtils:
         except Exception as e:
             logging.info('Exception Occured in load_object function utils')
             raise CustomException(e,sys)
-        
-
-        
+   
