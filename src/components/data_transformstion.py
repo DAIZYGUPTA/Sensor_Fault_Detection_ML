7473f094576e317fb66cbf8cfe5bfd52424a392f
DAIZYGUPTA <guptadaizy2495@gmail.com>
2026-01-17 17:50:40 +0530

    created model_trainer module

--- src/components/data_transformstion.py
+++ src/components/data_transformstion.py
@@ -7,13 +7,11 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import RobustScaler, FunctionTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-
 from src.constant import *
 from src.exception import CustomException
 from src.logger import logging
 from src.utils.main_utils import MainUtils
 from dataclasses import dataclass
-
 
 @dataclass
 class DataTransformationConfig:
@@ -22,42 +20,32 @@ class DataTransformationConfig:
     transformed_test_file_path = os.path.join(artifact_dir,'test.npy')
     transformed_object_file_path = os.path.join(artifact_dir,'preprocessor.pkl')
 
-
 class DataTransformation:
     def __init__(self,feature_store_file_path):
         self.feature_store_file_path = feature_store_file_path
-
         self.data_transformation_config = DataTransformationConfig()
-
         self.utils = MainUtils()
 
     @staticmethod
     def get_data(feature_store_file_path: str) ->pd.DataFrame:
-
         try:
 
             data = pd.read_csv(feature_store_file_path)
-
             data.rename(columns={"Good/Bad": TARGET_COLUMN}, inplace=True)
-
             return data
         except Exception as e:
             raise CustomException(e,sys)
         
     def get_data_transformer_object(self):
-
         try:
-
             imputer_step = ('imputer',SimpleImputer(strategy='constant', fill_value=0))
             scaler_step = ('scaler',RobustScaler())
-
             preprocessor = Pipeline(
                 steps=[
                     imputer_step,
                     scaler_step
                 ]
             )
-
             return preprocessor
         except Exception as e:
             raise CustomException(e,sys)
@@ -68,14 +56,10 @@ class DataTransformation:
 
         try:
             dataframe = self.get_data(feature_store_file_path=self.feature_store_file_path)
-
             X=dataframe.drop(columns= TARGET_COLUMN)
             y= np.where(dataframe[TARGET_COLUMN]==-1,0,1)
-
             X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
-
             preprocessor = self.get_data_transformer_object()
-
             X_train_scaled = preprocessor.fit_transform(X_train)
             X_test_scaled = preprocessor.transform(X_test)
 
